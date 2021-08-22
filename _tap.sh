3e4fc80a402d885850fb0e217292014722c133ee
James Arthur <thruflo@gmail.com>
2021-08-22 17:39:44 +0200

    release: autogenerate the homebrew tap formula.

---
+++ _tap.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+set -x
+
+rm -rf .relenv
+virtualenv --python=/usr/local/bin/python3.9 .relenv
+
+./.relenv/bin/pip install -r requirements.txt
+./.relenv/bin/python setup.py develop
+./.relenv/bin/pip freeze > .relenv/deps.txt
+
+python _tap_formula.py > ../homebrew-tap/Formula/sparse-cli.rb
+
+rm -rf .relenv
---
+++ _tap_formula.py
@@ -0,0 +1,64 @@
+"""Dynamically generate the contents of the `sparse/tap/sparse-cli` brew formula."""
+
+import pymustache
+import subprocess
+
+with open('VERSION') as f:
+    version = f.read().strip()
+
+with open('.relenv/deps.txt') as f:
+    deps = f.readlines()
+
+with open('_tap_formula.rb.tmpl') as f:
+    template = f.read()
+
+def sha_hash(file_path):
+    out = subprocess.check_output(['openssl', 'dgst', '-sha256', file_path])
+
+    return out.decode('utf-8').strip().split(' ')[-1]
+
+def download_url(package_name, version):
+    first_letter = package_name[0:1]
+
+    return f'https://pypi.io/packages/source/{ first_letter }/{ package_name }/{ package_name}-{version}.tar.gz'
+
+def render():
+    context = {
+        'version': version,
+        'url': download_url('sparse-cli', version),
+        'hash': sha_hash(f'dist/sparse-cli-{version}.tar.gz'),
+        'resources': []
+    }
+
+    for dep in deps:
+        if dep.startswith('sparse') or dep.startswith('-e'):
+            continue
+
+        dep = dep.strip()
+        dep_parts = dep.split('==')
+        dep_name = dep_parts[0]
+        dep_version = dep_parts[1]
+
+        dep_url = download_url(dep_name, dep_version)
+        download_path = f'./.relenv/{dep_name}'
+
+        subprocess.run(['wget', dep_url, '-qO', download_path])
+        dep_hash = sha_hash(download_path)
+
+        resource = {
+            'name': dep_name,
+            'url': dep_url,
+            'hash': dep_hash
+        }
+
+        context['resources'].append(resource)
+
+    return pymustache.render(template, context).strip()
+
+def main():
+    output = render()
+
+    print(output)
+
+if __name__ == '__main__':
+    main()
